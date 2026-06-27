478e0de03c51b6f58e47112858412c1d23e37967
Jerlin <20jannett2006@gmail.com>
2026-06-27 11:31:51 +0530

    FraudShield deployment

--- app.py
+++ app.py
@@ -483,4 +483,5 @@ def download():
 
 if __name__ == "__main__":
     print("🚀 http://127.0.0.1:5000")
-    app.run(debug=False)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=False)
